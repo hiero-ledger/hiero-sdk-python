ceab48445368c14dff6a18bc01d15450d21585f4
LukeBair <74842241+lukebair@users.noreply.github.com>
2025-05-14 12:43:38 -0600

    cleaned up
    Signed-off-by: LukeBair <74842241+LukeBair@users.noreply.github.com>

--- src/hiero_sdk_python/__init__.py
+++ src/hiero_sdk_python/__init__.py
@@ -15,6 +15,7 @@ from .tokens.token_create_transaction import TokenCreateTransaction
 from .tokens.token_associate_transaction import TokenAssociateTransaction
 from .tokens.token_dissociate_transaction import TokenDissociateTransaction
 from .tokens.token_delete_transaction import TokenDeleteTransaction
+from .tokens.token_info import TokenInfo
 from .tokens.token_mint_transaction import TokenMintTransaction
 from .tokens.token_freeze_transaction import TokenFreezeTransaction
 from .tokens.token_unfreeze_transaction import TokenUnfreezeTransaction
@@ -74,6 +75,7 @@ __all__ = [
     "TokenUnfreezeTransaction",
     "TokenId",
     "NftId",
+    "TokenInfo",
 
     # Transaction
     "TransferTransaction",
@@ -98,5 +100,6 @@ __all__ = [
 
     "Hbar",
     "ResponseCode",
-    "Timestamp"
+    "Timestamp",
+    "Duration"
 ]
