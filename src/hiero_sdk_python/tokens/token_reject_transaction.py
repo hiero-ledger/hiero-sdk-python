63a4c5e85389330820f1fd4af85c0ae8d9c49a60
dosi <dosi.kolev@limechain.tech>
2025-05-19 16:17:12 +0300

    feat: implement TokenRejectTransaction class
    Signed-off-by: dosi <dosi.kolev@limechain.tech>

---
+++ src/hiero_sdk_python/tokens/token_reject_transaction.py
@@ -0,0 +1,87 @@
+from hiero_sdk_python.hapi.services.token_reject_pb2 import TokenReference, TokenRejectTransactionBody
+from hiero_sdk_python.tokens.nft_id import NftId
+from hiero_sdk_python.tokens.token_id import TokenId
+from hiero_sdk_python.account.account_id import AccountId
+from hiero_sdk_python.transaction.transaction import Transaction
+from hiero_sdk_python.channels import _Channel
+from hiero_sdk_python.executable import _Method
+
+class TokenRejectTransaction(Transaction):
+    """
+    Represents a token reject transaction on the Hedera network.
+    
+    This transaction rejects a token transfer.
+    
+    Inherits from the base Transaction class and implements the required methods
+    to build and execute a token reject transaction.
+    """
+    def __init__(self, owner_id=None, token_ids=[], nft_ids=[]):
+        """
+        Initializes a new TokenRejectTransaction instance with optional owner_id, token_ids, and nft_ids.
+
+        Args:
+            owner_id (AccountId, optional): The ID of the account to reject the token transfer.
+            token_ids (list[TokenId], optional): The IDs of the tokens to reject.
+            nft_ids (list[int], optional): The serial numbers of NFTs to reject.
+        """
+        super().__init__()
+        self.owner_id : AccountId = owner_id
+        self.token_ids : list[TokenId] = token_ids
+        self.nft_ids : list[NftId] = nft_ids
+        
+    def set_owner_id(self, owner_id):
+        self._require_not_frozen()
+        self.owner_id = owner_id
+        return self
+
+    def set_token_ids(self, token_ids):
+        self._require_not_frozen()
+        self.token_ids = token_ids
+        return self
+    
+    def set_nft_ids(self, nft_ids):
+        self._require_not_frozen()
+        self.nft_ids = nft_ids
+        return self
+    
+    def build_transaction_body(self):
+        """
+        Builds and returns the protobuf transaction body for token reject.
+
+        Returns:
+            TransactionBody: The protobuf transaction body containing the token reject details.
+        """
+        token_references = []
+        for token_id in self.token_ids:
+            token_references.append(TokenReference(fungible_token=token_id.to_proto()))
+        for nft_id in self.nft_ids:
+            token_references.append(TokenReference(nft=nft_id.to_proto()))
+        
+        token_reject_body = TokenRejectTransactionBody(
+            owner=self.owner_id and self.owner_id.to_proto(),
+            rejections=token_references
+        )
+        transaction_body = self.build_base_transaction_body()
+        transaction_body.tokenReject.CopyFrom(token_reject_body)
+        return transaction_body
+    
+    def _get_method(self, channel: _Channel) -> _Method:
+        return _Method(
+            transaction_func=channel.token.rejectToken,
+            query_func=None
+        )
+    
+    def _from_proto(self, proto: TokenRejectTransactionBody):
+        """
+        Deserializes a TokenRejectTransactionBody from a protobuf object.
+
+        Args:
+            proto (TokenRejectTransactionBody): The protobuf object to deserialize.
+
+        Returns:
+            TokenRejectTransaction: Returns self for method chaining.
+        """
+        self.owner_id = AccountId.from_proto(proto.owner)
+        self.token_ids = [TokenId.from_proto(token_id) for token_id in proto.tokenIds]
+        self.nft_ids = [NftId.from_proto(nft_id) for nft_id in proto.nftIds]
+        return self
