77f1de2cfa44cdd684f312f4921824a68be6f603
Manish Dait <daitmanish88@gmail.com>
2025-06-16 21:41:10 +0530

    added AbstractTokenTransferTransaction
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

---
+++ src/hiero_sdk_python/tokens/abstract_token_transfer_transaction.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+from hiero_sdk_python.tokens.token_nft_transfer import TokenNftTransfer
+from hiero_sdk_python.tokens.token_transfer import TokenTransfer
+from hiero_sdk_python.tokens.token_transfer_list import TokenTransferList
+from hiero_sdk_python.transaction.transaction import Transaction
+
+class AbstractTokenTransferTransaction(Transaction):
+    def __init__(self):
+        """
+        Initializes a new ABstractTokenTransferTransaction instance.
+        """
+        super().__init__()
+        self.token_transfers: list[TokenTransfer] = []
+        self.nft_transfers: list[TokenNftTransfer] = []
+        self._default_transaction_fee = 100_000_000
+
+
+    def build_token_transfer_list(self) -> 'list[TokenTransferList]':
+        """
+        Aggregates all individual fungible token transfers and NFT transfers into
+        a list of TokenTransferList objects, where each TokenTransferList groups
+        transfers for a specific token ID.
+
+        Returns:
+            list[TokenTransferList]: A list of TokenTransferList objects
+        """
+        transfer_list = defaultdict(TokenTransferList)
+
+        for token_transfer in self.token_transfers:
+            if token_transfer.token_id not in transfer_list:
+                transfer_list[token_transfer.token_id] = TokenTransferList(
+                    token_transfer.token_id, 
+                    expected_decimals=token_transfer.expected_decimals
+                )            
+
+            transfer_list[token_transfer.token_id].add_token_transfer(token_transfer)
+
+        for nft_transfer in self.nft_transfers:
+            if nft_transfer.token_id not in transfer_list:
+                transfer_list[nft_transfer.token_id] = TokenTransferList(
+                    nft_transfer.token_id 
+                )
+
+            transfer_list[nft_transfer.token_id].add_nft_transfer(nft_transfer)
+
+        return list(transfer_list.values())
