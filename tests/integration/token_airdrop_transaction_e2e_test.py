6c4c96a1e11ee23bb7540eca8f72569d4c8fa710
Manish Dait <daitmanish88@gmail.com>
2025-06-19 20:57:58 +0530

    added extra checks to check the token balance
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- tests/integration/token_airdrop_transaction_e2e_test.py
+++ tests/integration/token_airdrop_transaction_e2e_test.py
@@ -1,16 +1,16 @@
+import pytest
+
+from hiero_sdk_python.query.account_balance_query import CryptoGetAccountBalanceQuery
 from hiero_sdk_python.tokens.token_airdrop_transaction import TokenAirdropTransaction
 from hiero_sdk_python.tokens.token_associate_transaction import TokenAssociateTransaction
 from hiero_sdk_python.tokens.token_nft_transfer import TokenNftTransfer
 from hiero_sdk_python.tokens.token_transfer import TokenTransfer
-import pytest
-
 from hiero_sdk_python.account.account_create_transaction import AccountCreateTransaction
 from hiero_sdk_python.crypto.private_key import PrivateKey
 from hiero_sdk_python.hbar import Hbar
 from hiero_sdk_python.tokens.token_mint_transaction import TokenMintTransaction
 from hiero_sdk_python.response_code import ResponseCode
 from tests.integration.utils_for_test import IntegrationTestEnv, create_fungible_token, create_nft_token
-
 
 @pytest.mark.integration
 def test_integration_token_airdrop_transaction_can_execute():
@@ -36,6 +36,8 @@ def test_integration_token_airdrop_transaction_can_execute():
         account_id = receipt.accountId
         assert account_id is not None
         
+        balance_before_tx = CryptoGetAccountBalanceQuery(account_id=account_id).execute(env.client)
+
         token_id = create_fungible_token(env)
         assert token_id is not None
 
@@ -73,6 +75,17 @@ def test_integration_token_airdrop_transaction_can_execute():
         airdrop_tx.sign(env.client.operator_private_key)
         airdrop_receipt = airdrop_tx.execute(env.client)
 
+        balance_after_tx = CryptoGetAccountBalanceQuery(account_id=account_id).execute(env.client)
+
         assert airdrop_receipt.status == ResponseCode.SUCCESS, f"Token airdrop failed with status: {ResponseCode(airdrop_receipt.status).name}"
+        
+        assert balance_before_tx.token_balances == {}
+        assert len(balance_after_tx.token_balances) == 2
+        
+        assert token_id in balance_after_tx.token_balances
+        assert balance_after_tx.token_balances[token_id] == 1
+
+        assert nft_id in balance_after_tx.token_balances
+        assert balance_after_tx.token_balances[nft_id] == 1
     finally:
         env.close() 
