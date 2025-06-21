499185c1d896600cd6f4bce60f7196e7749772d9
Manish Dait <daitmanish88@gmail.com>
2025-06-21 10:12:37 +0530

    fix some tests
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- tests/unit/test_token_transfer_list.py
+++ tests/unit/test_token_transfer_list.py
@@ -1,7 +1,7 @@
-from hiero_sdk_python.tokens.token_nft_transfer import TokenNftTransfer
-from hiero_sdk_python.tokens.token_transfer import TokenTransfer
 import pytest
 
+from hiero_sdk_python.tokens.token_nft_transfer import TokenNftTransfer
+from hiero_sdk_python.tokens.token_transfer import TokenTransfer
 from hiero_sdk_python.tokens.token_transfer_list import TokenTransferList
 
 pytestmark = pytest.mark.unit
@@ -68,25 +68,27 @@ def test_token_transfer_list_add_token_transfer(mock_account_ids):
 def test_token_transfer_list_add_nft_transfer(mock_account_ids):
     """Test TokenTransferList add_nft_transfer function"""
     account_id1, account_id2, _, token_id, _ = mock_account_ids
-    nft_transfers = [TokenNftTransfer(token_id, account_id1, account_id2, 10)]
+    transfer = TokenNftTransfer(token_id, account_id1, account_id2, 10)
 
     token_transfer_list = TokenTransferList(
         token=token_id,
     )
 
-    token_transfer_list.add_nft_transfer(nft_transfers)
+    token_transfer_list.add_nft_transfer(transfer)
 
     assert token_transfer_list.nft_transfers is not None
-    assert token_transfer_list.nft_transfers == [nft_transfers]
+    assert token_transfer_list.nft_transfers == [transfer]
 
 def test_to_proto(mock_account_ids):
     """Test converting TokenTransferList to protobuf object"""
-    sender_id, reciver_id, _, token_id, _ = mock_account_ids
+    sender_id, reciver_id, _, token_id_1, token_id_2 = mock_account_ids
     expected_decimals = 1
-    transfers = [TokenTransfer(token_id, sender_id, -10), TokenTransfer(token_id, reciver_id, 10)]
+    transfers = [TokenTransfer(token_id_1, sender_id, -10), TokenTransfer(token_id_1, reciver_id, 10)]
+    nft_transfers = [TokenNftTransfer(token_id_2, sender_id, reciver_id, 1)]
+    
     
     token_transfer_list = TokenTransferList(
-        token=token_id,
+        token=token_id_1,
         transfers=transfers,
         expected_decimals=expected_decimals
     )
@@ -94,12 +96,32 @@ def test_to_proto(mock_account_ids):
     # Convert to protobuf 
     proto = token_transfer_list._to_proto()
 
-    assert proto.token.shardNum == token_id.shard
-    assert proto.token.realmNum == token_id.realm
-    assert proto.token.tokenNum == token_id.num 
+    assert proto.token.shardNum == token_id_1.shard
+    assert proto.token.realmNum == token_id_1.realm
+    assert proto.token.tokenNum == token_id_1.num 
     assert proto.expected_decimals.value == expected_decimals
     assert len(proto.transfers) == 2
+    assert len(proto.nftTransfers) == 0
     assert proto.transfers[0].accountID.accountNum == sender_id.num
     assert proto.transfers[0].amount == -10
     assert proto.transfers[1].accountID.accountNum == reciver_id.num
     assert proto.transfers[1].amount == 10
+
+    # Check for NFT Transfer
+    nft_transfer_list = TokenTransferList(
+        token=token_id_2,
+        nft_transfers=nft_transfers,
+    )
+
+    # Convert to protobuf 
+    proto = nft_transfer_list._to_proto()
+
+    assert proto.token.shardNum == token_id_2.shard
+    assert proto.token.realmNum == token_id_2.realm
+    assert proto.token.tokenNum == token_id_2.num 
+    assert proto.expected_decimals.value == 0
+    assert len(proto.transfers) == 0
+    assert len(proto.nftTransfers) == 1
+    assert proto.nftTransfers[0].senderAccountID.accountNum == sender_id.num
+    assert proto.nftTransfers[0].receiverAccountID.accountNum == reciver_id.num
+    assert proto.nftTransfers[0].serialNumber == 1
