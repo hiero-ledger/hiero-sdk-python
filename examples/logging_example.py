fd4fcb9faced53353f992e1f844cd055799aaf42
exploreriii <133720349+exploreriii@users.noreply.github.com>
2025-07-06 17:48:32 +0100

    revert: example env
    Signed-off-by: exploreriii <133720349+exploreriii@users.noreply.github.com>

--- examples/logging_example.py
+++ examples/logging_example.py
@@ -11,7 +11,7 @@ from hiero_sdk_python import (
     LogLevel,
 )
 
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env.example"))
+load_dotenv()
 
 def show_logging_workflow():
     """Function to demonstrate logging functionality in the Hiero SDK."""
