b3196c7ab88692909de2c4cc8647fb4d534b95cd
exploreriii <133720349+exploreriii@users.noreply.github.com>
2025-07-12 22:01:44 +0100

    refactor: test token info query with snake casing
    Signed-off-by: exploreriii <133720349+exploreriii@users.noreply.github.com>

--- tests/unit/test_token_info_query.py
+++ tests/unit/test_token_info_query.py
@@ -116,11 +116,11 @@ def test_token_info_query_execute(mock_account_ids, private_key):
         assert result.totalSupply == 100
         assert result.maxSupply == 10000
         assert result.treasury == account_id
-        assert result.autoRenewAccount == renew_account_id
-        assert result.defaultFreezeStatus == 0
-        assert result.defaultKycStatus == 0
-        assert result.adminKey.to_bytes_raw() == private_key.public_key().to_bytes_raw()
-        assert result.kycKey.to_bytes_raw() == private_key.public_key().to_bytes_raw()
-        assert result.wipeKey.to_bytes_raw() == private_key.public_key().to_bytes_raw()
-        assert result.supplyKey == None
-        assert result.freezeKey == None
+        assert result.auto_renew_account == renew_account_id
+        assert result.default_freeze_status == 0
+        assert result.default_freeze_status == 0
+        assert result.admin_key.to_bytes_raw() == private_key.public_key().to_bytes_raw()
+        assert result.kyc_key.to_bytes_raw() == private_key.public_key().to_bytes_raw()
+        assert result.wipe_key.to_bytes_raw() == private_key.public_key().to_bytes_raw()
+        assert result.supply_key == None
+        assert result.freeze_key == None
