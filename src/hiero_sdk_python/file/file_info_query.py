bfe65ebd460f9651f8218a33f33d1b93df9f0780
dosi <dosi.kolev@limechain.tech>
2025-07-22 13:13:42 +0300

    style: update linting and type hinting in file info query
    Signed-off-by: dosi <dosi.kolev@limechain.tech>

--- src/hiero_sdk_python/file/file_info_query.py
+++ src/hiero_sdk_python/file/file_info_query.py
@@ -1,22 +1,29 @@
+"""
+Query to get information about a file on the network.
+"""
+
 import traceback
 from typing import Optional
-from hiero_sdk_python.query.query import Query
-from hiero_sdk_python.hapi.services import query_pb2, file_get_info_pb2
-from hiero_sdk_python.hapi.services.file_get_info_pb2 import FileGetInfoResponse
-from hiero_sdk_python.executable import _Method
-from hiero_sdk_python.channels import _Channel
 
+from hiero_sdk_python.channels import _Channel
+from hiero_sdk_python.client.client import Client
+from hiero_sdk_python.executable import _Method
 from hiero_sdk_python.file.file_id import FileId
 from hiero_sdk_python.file.file_info import FileInfo
+from hiero_sdk_python.hapi.services import file_get_info_pb2, query_pb2, response_pb2
+from hiero_sdk_python.hapi.services.file_get_info_pb2 import FileGetInfoResponse
+from hiero_sdk_python.query.query import Query
+
 
 class FileInfoQuery(Query):
     """
     A query to retrieve information about a specific File.
-    
-    This class constructs and executes a query to retrieve information 
+
+    This class constructs and executes a query to retrieve information
     about a file on the network, including the file's properties and settings.
-    
+
     """
+
     def __init__(self, file_id: Optional[FileId] = None) -> None:
         """
         Initializes a new FileInfoQuery instance with an optional file_id.
@@ -25,17 +32,17 @@ class FileInfoQuery(Query):
             file_id (Optional[FileId], optional): The ID of the file to query.
         """
         super().__init__()
-        self.file_id : Optional[FileId] = file_id
+        self.file_id: Optional[FileId] = file_id
 
-    def set_file_id(self, file_id: Optional[FileId]) -> 'FileInfoQuery':
+    def set_file_id(self, file_id: Optional[FileId]) -> "FileInfoQuery":
         """
-        Sets the ID of the file to query. 
+        Sets the ID of the file to query.
 
-        Args: 
-            file_id (Optional[FileId]): The ID of the file. 
+        Args:
+            file_id (Optional[FileId]): The ID of the file.
 
         Returns:
-            FileInfoQuery: Returns self for method chaining. 
+            FileInfoQuery: Returns self for method chaining.
         """
         self.file_id = file_id
         return self
@@ -43,7 +50,7 @@ class FileInfoQuery(Query):
     def _make_request(self) -> query_pb2.Query:
         """
         Constructs the protobuf request for the query.
-        
+
         Builds a FileGetInfoQuery protobuf message with the
         appropriate header and file ID.
 
@@ -66,7 +73,7 @@ class FileInfoQuery(Query):
 
             query = query_pb2.Query()
             query.fileGetInfo.CopyFrom(file_info_query)
-                  
+
             return query
         except Exception as e:
             print(f"Exception in _make_request: {e}")
@@ -76,7 +83,7 @@ class FileInfoQuery(Query):
     def _get_method(self, channel: _Channel) -> _Method:
         """
         Returns the appropriate gRPC method for the file info query.
-        
+
         Implements the abstract method from Query to provide the specific
         gRPC method for getting file information.
 
@@ -86,19 +93,17 @@ class FileInfoQuery(Query):
         Returns:
             _Method: The method wrapper containing the query function
         """
-        return _Method(
-            transaction_func=None,
-            query_func=channel.file.getFileInfo
-        )
+        return _Method(transaction_func=None, query_func=channel.file.getFileInfo)
 
-    def execute(self, client) -> FileInfo:
+    def execute(self, client: Client) -> FileInfo:
         """
         Executes the file info query.
-        
+
         Sends the query to the Hedera network and processes the response
         to return a FileInfo object.
 
-        This function delegates the core logic to `_execute()`, and may propagate exceptions raised by it.
+        This function delegates the core logic to `_execute()`, and may propagate
+        exceptions raised by it.
 
         Args:
             client (Client): The client instance to use for execution
@@ -116,16 +121,18 @@ class FileInfoQuery(Query):
 
         return FileInfo._from_proto(response.fileGetInfo.fileInfo)
 
-    def _get_query_response(self, response) -> FileGetInfoResponse.FileInfo:
+    def _get_query_response(
+        self, response: response_pb2.Response
+    ) -> FileGetInfoResponse.FileInfo:
         """
         Extracts the file info response from the full response.
-        
+
         Implements the abstract method from Query to extract the
         specific file info response object.
-        
+
         Args:
             response: The full response from the network
-            
+
         Returns:
             The file get info response object
         """
