84e4a8c63232a62c068a91bb9ed81c89d5a69caf
exploreriii <133720349+exploreriii@users.noreply.github.com>
2025-08-17 11:38:21 +0100

    feat: freezing account id
    Signed-off-by: exploreriii <133720349+exploreriii@users.noreply.github.com>

--- src/hiero_sdk_python/account/account_id.py
+++ src/hiero_sdk_python/account/account_id.py
@@ -1,23 +1,12 @@
+from dataclasses import dataclass
 from typing import List
 from hiero_sdk_python.hapi.services import basic_types_pb2
 
+@dataclass(frozen=True, eq=True)
 class AccountId:
-    def __init__(
-        self,
-        shard: int = 0, 
-        realm: int = 0, 
-        num: int = 0
-    ) -> None:
-        """
-        Initialize a new AccountId instance.
-        Args:
-            shard (int): The shard number of the account.
-            realm (int): The realm number of the account.
-            num (int): The account number.
-        """
-        self.shard = shard
-        self.realm = realm
-        self.num = num
+    shard: int = 0
+    realm: int = 0
+    num: int = 0
 
     @classmethod
     def from_string(cls, account_id_str: str) -> "AccountId":
@@ -66,24 +55,8 @@ class AccountId:
         """
         return f"{self.shard}.{self.realm}.{self.num}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """
         Returns the repr representation of the AccountId.
         """
         return f"AccountId(shard={self.shard}, realm={self.realm}, num={self.num})"
-
-    def __eq__(self, other: object) -> bool:
-        """
-        Checks equality between two AccountId instances.
-        Args:
-            other (object): The object to compare with.
-        Returns:
-            bool: True if both instances are equal, False otherwise.
-        """
-        if not isinstance(other, AccountId):
-            return False
-        return (self.shard, self.realm, self.num) == (other.shard, other.realm, other.num)
-
-    def __hash__(self) -> int:
-        """Returns a hash value for the AccountId instance."""
-        return hash((self.shard, self.realm, self.num))
