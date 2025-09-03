1baca07e61c25b97e2bb94a9eca645190ce93f89
Manish Dait <daitmanish88@gmail.com>
2025-09-03 13:49:33 +0530

    fix: updated topic_update.py to use dynamic topicId
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- examples/topic_update.py
+++ examples/topic_update.py
@@ -6,23 +6,51 @@ from hiero_sdk_python import (
     Client,
     AccountId,
     PrivateKey,
-    TopicId,
     TopicUpdateTransaction,
     Network,
+    TopicCreateTransaction,
+    ResponseCode
 )
 
 load_dotenv()
 
 def update_topic(new_memo):
+    """
+    A example to create a topic and then update it.
+    """
+    # Config Client
+    print("Connecting to Hedera testnet...")
     network = Network(network='testnet')
     client = Client(network)
 
-    operator_id = AccountId.from_string(os.getenv('OPERATOR_ID'))
-    operator_key = PrivateKey.from_string_ed25519(os.getenv('OPERATOR_KEY'))
-    topic_id = TopicId.from_string(os.getenv('TOPIC_ID'))
+    try:
+        operator_id = AccountId.from_string(os.getenv('OPERATOR_ID'))
+        operator_key = PrivateKey.from_string(os.getenv('OPERATOR_KEY'))
+        client.set_operator(operator_id, operator_key)
+    except (TypeError, ValueError):
+        print("❌ Error: Creating client, Please check your .env file")
+        sys.exit(1)
 
-    client.set_operator(operator_id, operator_key)
+    # Create a new Topic
+    print("\nCreating a Topic...")
+    try:
+        topic_tx = (
+            TopicCreateTransaction(
+                memo="Python SDK created topic",
+                admin_key=operator_key.public_key()
+            )
+            .freeze_with(client)
+            .sign(operator_key)
+        )
+        topic_receipt = topic_tx.execute(client)
+        topic_id = topic_receipt.topic_id
+        print(f"✅ Success! Created topic: {topic_id}")
+    except Exception as e:
+        print(f"❌ Error: Creating topic: {e}")
+        sys.exit(1)
 
+    # Update the Topic
+    print("\nUpdating Topic...")
     transaction = (
         TopicUpdateTransaction(topic_id=topic_id, memo=new_memo)
         .freeze_with(client)
@@ -31,9 +59,12 @@ def update_topic(new_memo):
 
     try:
         receipt = transaction.execute(client)
-        print(f"Topic {topic_id} updated with new memo: {new_memo}")
+        print(f"Topic Update Transaction completed: "
+              f"(status: {ResponseCode(receipt.status).name}, "
+              f"transaction_id: {receipt.transaction_id})")
+        print(f"✅ Success! Topic {topic_id} updated with new memo: {new_memo}")
     except Exception as e:
-        print(f"Topic update failed: {str(e)}")
+        print(f"❌ Error: Topic update failed: {str(e)}")
         sys.exit(1)
 
 if __name__ == "__main__":
