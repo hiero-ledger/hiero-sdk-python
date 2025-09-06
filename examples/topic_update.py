exploreriii/hiero_sdk_python

chore: update to general key types in examples
Signed-off-by: exploreriii <133720349+exploreriii@users.noreply.github.com>

examples/topic_update.py
@@ -18,7 +18,7 @@ def update_topic(new_memo):
     client = Client(network)
 
     operator_id = AccountId.from_string(os.getenv('OPERATOR_ID'))
-    operator_key = PrivateKey.from_string_ed25519(os.getenv('OPERATOR_KEY'))
+    operator_key = PrivateKey.from_string(os.getenv('OPERATOR_KEY'))
     topic_id = TopicId.from_string(os.getenv('TOPIC_ID'))
 
     client.set_operator(operator_id, operator_key)

hiero-ledger/hiero-sdk-python

examples/topic_update.py
@@ -55,8 +55,9 @@ def update_topic(new_memo):
     # Config Client
     client, _, operator_key = setup_client()
 
-    # Create a new Topic
-    topic_id = create_topic(client, operator_key)
+    operator_id = AccountId.from_string(os.getenv('OPERATOR_ID'))
+    operator_key = PrivateKey.from_string_ed25519(os.getenv('OPERATOR_KEY'))
+    topic_id = TopicId.from_string(os.getenv('TOPIC_ID'))
 
     # Update the Topic
     print("\nSTEP 2: Updating Topic...")