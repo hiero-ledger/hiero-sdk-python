4b3047b4c86f403f8da3e82b1fd02dfc27645212
nadinedelia <nadine.loepfe@swirldslabs.com>
2025-09-22 18:25:35 +0400

    updating example script
    Signed-off-by: nadinedelia <nadine.loepfe@swirldslabs.com>

--- examples/query_schedule_info.py
+++ examples/query_schedule_info.py
@@ -8,12 +8,7 @@ import sys
 
 from dotenv import load_dotenv
 
-from hiero_sdk_python import AccountId, Client, Hbar, Network, PrivateKey
-from hiero_sdk_python.account.account_create_transaction import AccountCreateTransaction
-from hiero_sdk_python.response_code import ResponseCode
-from hiero_sdk_python.schedule.schedule_info_query import ScheduleInfoQuery
-from hiero_sdk_python.timestamp import Timestamp
-from hiero_sdk_python.transaction.transfer_transaction import TransferTransaction
+from hiero_sdk_python import AccountId, Client, Hbar, Network, PrivateKey, AccountCreateTransaction, ResponseCode, ScheduleInfoQuery, Timestamp, TransferTransaction
 
 load_dotenv()
 
