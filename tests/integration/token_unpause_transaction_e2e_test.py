5b18934462b4318e62ea71f76ee17a1250a43f4e
Manish Dait <daitmanish88@gmail.com>
2025-10-29 21:49:47 +0530

    chore:improve integration test fro token_unpause_transaction
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- tests/integration/token_unpause_transaction_e2e_test.py
+++ tests/integration/token_unpause_transaction_e2e_test.py
@@ -1,7 +1,9 @@
-from pytest import fixture, mark
+from pytest import fixture
+import pytest
 from hiero_sdk_python.crypto.private_key import PrivateKey
 from hiero_sdk_python.query.token_info_query import TokenInfoQuery
 from hiero_sdk_python.response_code import ResponseCode
+from hiero_sdk_python.tokens.token_id import TokenId
 from hiero_sdk_python.tokens.token_pause_transaction import TokenPauseTransaction
 from hiero_sdk_python.tokens.token_unpause_transaction import TokenUnpauseTransaction
 from tests.integration.utils_for_test import env, create_fungible_token
@@ -10,12 +12,14 @@ pause_key = PrivateKey.generate()
 
 @fixture
 def pausable_token(env):
+    """"Fixture to create a token that supports pausing."""
     return create_fungible_token(
         env, 
         opts=[lambda tx: tx.set_pause_key(pause_key)]
     )
 
-def pause_token(env, token_id):
+def pause_token(env, token_id: TokenId):
+    """Helper function to pause the token with the given token_id."""
     token_pasue_tx = (
         TokenPauseTransaction()
         .set_token_id(token_id)
@@ -25,24 +29,80 @@ def pause_token(env, token_id):
 
     return token_pasue_tx.execute(env.client)
 
+def get_pause_status(env, token_id: TokenId):
+    """Helper to query and return the pause status of a token."""
+    token_info = TokenInfoQuery().set_token_id(token_id).execute(env.client)
+    return token_info.pause_status.name
 
-def test_integration_token_unpause_transaction(env, pausable_token):
-    pause_tx_recipt = pause_token(env, pausable_token)
-        
-    assert pause_tx_recipt.status == ResponseCode.SUCCESS
+def test_unpause_token(env, pausable_token):
+    """Test successful unpause of a paused token."""
+    pause_receipt = pause_token(env, pausable_token)
 
-    info1 = TokenInfoQuery().set_token_id(pausable_token).execute(env.client)
-    assert info1.pause_status.name == "PAUSED"
+    assert pause_receipt.status == ResponseCode.SUCCESS 
+    assert get_pause_status(env, pausable_token) == "PAUSED"
 
-    token_unpause_tx = (
+    unpause_tx = (
         TokenUnpauseTransaction()
         .set_token_id(pausable_token)
         .freeze_with(env.client)
         .sign(pause_key)
     )
 
-    unpause_tx_recipt = token_unpause_tx.execute(env.client)
-    assert unpause_tx_recipt.status == ResponseCode.SUCCESS
+    unpause_receipt = unpause_tx.execute(env.client)
+    assert unpause_receipt.status == ResponseCode.SUCCESS
+    assert get_pause_status(env, pausable_token) == "UNPAUSED"
 
-    info2 = TokenInfoQuery().set_token_id(pausable_token).execute(env.client)
-    assert info2.pause_status.name == "UNPAUSED"
+
+def test_unpause_token_without_pause_key_signature(env, pausable_token):
+    """Test unpause transaction without pause key signature."""
+    pause_receipt = pause_token(env, pausable_token)
+
+    assert pause_receipt.status == ResponseCode.SUCCESS
+    assert get_pause_status(env, pausable_token) == "PAUSED"
+
+    unpause_tx = (
+        TokenUnpauseTransaction()
+        .set_token_id(pausable_token)
+        .freeze_with(env.client)
+    )
+
+    unpause_receipt = unpause_tx.execute(env.client)
+    assert unpause_receipt.status == ResponseCode.INVALID_SIGNATURE
+
+def test_unpause_token_with_invalid_pasue_key(env, pausable_token):
+    """Test unpause transaction with invalid pause key."""
+    pause_receipt = pause_token(env, pausable_token)
+
+    assert pause_receipt.status == ResponseCode.SUCCESS
+    assert get_pause_status(env, pausable_token) == "PAUSED"
+
+    unpause_tx = (
+        TokenUnpauseTransaction()
+        .set_token_id(pausable_token)
+        .freeze_with(env.client)
+        .sign(PrivateKey.generate())
+    )
+
+    unpause_receipt = unpause_tx.execute(env.client)
+    assert unpause_receipt.status == ResponseCode.INVALID_SIGNATURE
+
+def test_unpause_token_when_token_id_not_set(env):
+    """Test unpause transaction when token_id is not provided."""
+
+    unpause_tx = TokenUnpauseTransaction()
+    with pytest.raises(ValueError, match="Missing token ID"):
+        unpause_tx.freeze_with(env.client)
+
+def test_unpause_token_with_invalid_token_id(env):
+    """Test unpause transaction using an invalid token ID."""
+    token_id = TokenId(0, 0, 999)
+
+    unpause_tx = (
+        TokenUnpauseTransaction()
+        .set_token_id(token_id)
+        .freeze_with(env.client)
+        .sign(pause_key)
+    )
+
+    unpause_receipt = unpause_tx.execute(env.client)
+    assert unpause_receipt.status == ResponseCode.INVALID_TOKEN_ID
