c35b1caad03e026b22fa1ca67994c01494403553
exploreriii <133720349+exploreriii@users.noreply.github.com>
2025-10-30 20:51:47 +0000

    chore: rename airdrop examples, transactions and tests to be grouped
    Signed-off-by: exploreriii <133720349+exploreriii@users.noreply.github.com>

--- src/hiero_sdk_python/__init__.py
+++ src/hiero_sdk_python/__init__.py
@@ -33,9 +33,9 @@ from .tokens.token_grant_kyc_transaction import TokenGrantKycTransaction
 from .tokens.token_revoke_kyc_transaction import TokenRevokeKycTransaction
 from .tokens.token_update_transaction import TokenUpdateTransaction
 from .tokens.token_airdrop_transaction import TokenAirdropTransaction
-from .tokens.token_cancel_airdrop_transaction import TokenCancelAirdropTransaction
-from .tokens.pending_airdrop_id import PendingAirdropId
-from .tokens.pending_airdrop_record import PendingAirdropRecord
+from .tokens.token_airdrop_transaction_cancel import TokenCancelAirdropTransaction
+from .tokens.token_airdrop_pending_id import PendingAirdropId
+from .tokens.token_airdrop_pending_record import PendingAirdropRecord
 from .tokens.token_id import TokenId
 from .tokens.token_type import TokenType
 from .tokens.supply_type import SupplyType
