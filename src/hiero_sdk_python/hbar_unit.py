8718653783ca9d59c07ff8d70f2173b96ef24181
Manish Dait <daitmanish88@gmail.com>
2025-11-09 00:16:52 +0530

    chore: added comments to the HbarUnit enum
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- src/hiero_sdk_python/hbar_unit.py
+++ src/hiero_sdk_python/hbar_unit.py
@@ -4,12 +4,25 @@ class HbarUnit(Enum):
     """
     Represents the various denominations of the HBAR (or network utility token).
     """
+    # 1 tinybar = base unit
     TINYBAR = ('tℏ', 1)
+
+    # 1 microbar = 100 tinybars
     MICROBAR = ('μℏ', 10**2)
+    
+    # 1 millibar = 100,000 tinybars
     MILLIBAR = ('mℏ', 10**5)
+    
+    # 1 hbar = 100,000,000 tinybars
     HBAR = ('ℏ', 10**8)
+
+    # 1 kilobar = 100,000,000,000 tinybars
     KILOBAR = ('kℏ', 10**11)
+    
+    # 1 megabar = 100,000,000,000,000 tinybars
     MEGABAR = ('Mℏ', 10**14)
+    
+    # 1 gigabar = 100,000,000,000,000,000 tinybars
     GIGABAR = ('Gℏ', 10**17)
 
     def __init__(self, symbol: str, tinybar: int):
