a316ae4d4bbf80e5ae04505aad05393372ffb795
undefinedIsMyLife <shinetina169@gmail.com>
2025-12-20 20:42:02 +0530

    fix: add PR issue link reminder workflow 3
    Signed-off-by: undefinedIsMyLife <shinetina169@gmail.com>

---
+++ .github/scripts/pr_missing_linked_issue.js
@@ -0,0 +1,31 @@
+module.exports = async ({ github, context }) => {
+  const body = context.payload.pull_request.body || "";
+  const regex = /(Fixes)\s+#\d+/i;
+
+  if (!regex.test(body)) {
+    await github.rest.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.issue.number,
+      body: [
+        `Hi @${context.payload.pull_request.user.login}, this is **LinkBot** 👋`,
+        ``,
+        `Linking pull requests to issues helps us significantly with reviewing pull requests and keeping the repository healthy.`,
+        ``,
+        `🚨 **This pull request does not have an issue linked.**`,
+        ``,
+        `Please link an issue using the following format:`,
+        `- Fixes #123`,
+        ``,
+        `📖 Guide:`,
+        `docs/sdk_developers/how_to_link_issue.md`,
+        ``,
+        `If no issue exists yet, please create one:`,
+        `docs/sdk_developers/creating_issues.md`,
+        ``,
+        `Thanks!`
+      ].join('\n')
+    });
+  }
+};
+
