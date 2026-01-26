a9338b3c900ac33746c61649a142754318b0d6e2
Akshay Sahu <akshaysahu@Akshays-MacBook-Pro.local>
2026-01-26 16:28:10 +0530

    format black tests/unit/nft_id_test.py
    Signed-off-by: Akshay Sahu <akshaysahu@Akshays-MacBook-Pro.local>

--- tests/unit/nft_id_test.py
+++ tests/unit/nft_id_test.py
@@ -5,42 +5,54 @@ from hiero_sdk_python.tokens.token_id import TokenId
 from hiero_sdk_python.hapi.services import basic_types_pb2
 
 pytestmark = pytest.mark.unit
+
+
 def test_nft_id():
-    #return true
+    # return true
     nftid_constructor_tokenid = TokenId(shard=0, realm=1, num=2)
-    nftid_constructor_test = NftId(token_id=nftid_constructor_tokenid, serial_number=1234)
+    nftid_constructor_test = NftId(
+        token_id=nftid_constructor_tokenid, serial_number=1234
+    )
 
     assert str(nftid_constructor_test) == "0.1.2/1234"
-    assert repr(nftid_constructor_test) == "NftId(token_id=TokenId(shard=0, realm=1, num=2, checksum=None), serial_number=1234)"
+    assert (
+        repr(nftid_constructor_test)
+        == "NftId(token_id=TokenId(shard=0, realm=1, num=2, checksum=None), serial_number=1234)"
+    )
     assert nftid_constructor_test._to_proto().__eq__(
         basic_types_pb2.NftID(
             token_ID=basic_types_pb2.TokenID(shardNum=0, realmNum=1, tokenNum=2),
-            serial_number=1234)
+            serial_number=1234,
+        )
     )
     assert NftId._from_proto(
         nft_id_proto=basic_types_pb2.NftID(
             token_ID=basic_types_pb2.TokenID(shardNum=0, realmNum=1, tokenNum=2),
-            serial_number=1234
+            serial_number=1234,
         )
     ).__eq__(nftid_constructor_test)
 
-    #return false
+    # return false
     with pytest.raises(TypeError):
         nftid_failed_constructor_tokenid1 = TokenId(shard=0, realm=1, num="A")
     with pytest.raises(TypeError):
         nftid_failed_constructor_tokenid = TokenId(shard=0, realm="b", num=1)
     with pytest.raises(TypeError):
-        nftid_failed_constructor_tokenid = TokenId(shard='c', realm=1, num=1)
+        nftid_failed_constructor_tokenid = TokenId(shard="c", realm=1, num=1)
     with pytest.raises(TypeError):
         nftid_failed_constructor = NftId(token_id=None, serial_number=1234)
     with pytest.raises(TypeError):
         nftid_failed_constructor = NftId(token_id=1234, serial_number=1234)
     with pytest.raises(TypeError):
-        nftid_failed_constructor = NftId(token_id=TokenId(shard=0, realm=1, num=0), serial_number="asdfasdfasdf")
+        nftid_failed_constructor = NftId(
+            token_id=TokenId(shard=0, realm=1, num=0), serial_number="asdfasdfasdf"
+        )
     with pytest.raises(ValueError):
-        nftid_failed_constructor = NftId(token_id=TokenId(shard=0, realm=1, num=0), serial_number=-1234)
+        nftid_failed_constructor = NftId(
+            token_id=TokenId(shard=0, realm=1, num=0), serial_number=-1234
+        )
 
-    #don't need to test protobuf cause its final and type checked
+    # don't need to test protobuf cause its final and type checked
     with pytest.raises(ValueError):
         NftId.from_string("")
 
@@ -65,6 +77,7 @@ def test_nft_id():
     fail_str = "a.3.3/19"
     with pytest.raises(ValueError):
         NftId.from_string(fail_str)
+
 
 def test_get_nft_id_with_checksum(mock_client):
     """Should return string with checksum when ledger id is provided."""
