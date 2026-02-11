4323d61309f1138fd67eecf01fa616ec1a2e792b
Manish Dait <daitmanish88@gmail.com>
2026-02-11 20:34:58 +0530

    chore: added some helper method
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- src/hiero_sdk_python/transaction/transaction.py
+++ src/hiero_sdk_python/transaction/transaction.py
@@ -406,7 +406,7 @@ class Transaction(_Executable):
 
         return any(sig_pair.pubKeyPrefix == public_key_bytes for sig_pair in sig_map.sigPair)
 
-    def build_transaction_body(self):
+    def build_transaction_body(self) -> transaction_pb2.Transaction:
         """
         Abstract method to build the transaction body.
 
@@ -894,3 +894,13 @@ class Transaction(_Executable):
         self.freeze_with(client)
         self.sign(client.operator_private_key)
         return self
+    
+    def get_size(self) -> int:
+        """Returns the total transaction size in bytes after protobuf encoding"""
+        self._require_frozen()
+        return self._make_request().ByteSize()
+    
+    def get_body_size(self) -> int:
+        """Returns just the transaction body size in bytes after encoding"""
+        self._require_frozen()
+        return self.build_transaction_body().ByteSize()
