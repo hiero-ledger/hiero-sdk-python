20995c6fd4e401d66a30aa72c998072424db78d1
Mounil Kanakhara <mounilkankhara@gmail.com>
2026-03-22 14:42:52 +0530

    fix: Pass None for child transaction IDs in receipt queries
    Signed-off-by: Mounil Kanakhara <mounilkankhara@gmail.com>

--- src/hiero_sdk_python/transaction/transaction_receipt.py
+++ src/hiero_sdk_python/transaction/transaction_receipt.py
@@ -242,16 +242,13 @@ class TransactionReceipt:
         return self._receipt_proto
 
     @classmethod
-    def _from_proto(
-        cls, proto: transaction_receipt_pb2.TransactionReceipt, transaction_id: TransactionId
-    ) -> TransactionReceipt:
+    def _from_proto(cls, proto: transaction_receipt_pb2.TransactionReceipt, transaction_id: Optional[TransactionId]) -> "TransactionReceipt":
         """
         Creates a TransactionReceipt instance from a protobuf TransactionReceipt object.
 
         Args:
             proto (transaction_receipt_pb2.TransactionReceipt): The protobuf TransactionReceipt object.
-            transaction_id (TransactionId): The transaction ID associated with this receipt.
-
+            transaction_id (Optional[TransactionId]): The transaction ID associated with this receipt. Can be None for child receipts.
         Returns:
             TransactionReceipt: A new instance of TransactionReceipt populated with data from the protobuf object.
         """
