d6e13f69a48a5e77bc5efb9b24500ea52ad9298b
Adityarya11 <arya050411@gmail.com>
2026-04-16 23:34:07 +0530

    removed valueError from accInfoQuery.
    Signed-off-by: Adityarya11 <arya050411@gmail.com>

--- src/hiero_sdk_python/query/account_info_query.py
+++ src/hiero_sdk_python/query/account_info_query.py
@@ -52,13 +52,9 @@ class AccountInfoQuery(Query):
             Query: The protobuf query message.
 
         Raises:
-            ValueError: If the account ID is not set.
             Exception: If any other error occurs during request construction.
         """
         try:
-            if not self.account_id:
-                raise ValueError("Account ID must be set before making the request.")
-
             query_header = self._make_request_header()
 
             crypto_info_query = crypto_get_info_pb2.CryptoGetInfoQuery()
