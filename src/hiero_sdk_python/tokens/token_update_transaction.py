0f9b3e39b9ea412868f6aefb94893380366136e8
Yuval Goihberg <yuval@blockaid.co>
2026-04-23 18:34:06 +0300

    fix: remove unused import
    Signed-off-by: Yuval Goihberg <yuval@blockaid.co>

--- src/hiero_sdk_python/tokens/token_update_transaction.py
+++ src/hiero_sdk_python/tokens/token_update_transaction.py
@@ -26,7 +26,6 @@ from hiero_sdk_python.tokens.token_id import TokenId
 from hiero_sdk_python.tokens.token_key_validation import TokenKeyValidation
 from hiero_sdk_python.transaction.transaction import Transaction
 from hiero_sdk_python.crypto.key import Key
-from hiero_sdk_python.crypto.public_key import PublicKey
 from hiero_sdk_python.utils.key_utils import key_to_proto
 
 
