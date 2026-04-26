b77dcc1dd7f51826f72b195642ad3ab3a4189cc3
IGwork <mounilkankhara@gmail.com>
2026-04-27 00:20:52 +0530

    fix: set explicit transaction fee in account records query integration tests
    Signed-off-by: IGwork <mounilkankhara@gmail.com>

--- tests/integration/account_records_query_e2e_test.py
+++ tests/integration/account_records_query_e2e_test.py
@@ -20,8 +20,10 @@ def test_integration_account_record_query_can_execute(env):
     account = env.create_account()
 
     # Perform transfer transaction
+    transfer_tx = TransferTransaction()
+    transfer_tx.transaction_fee = Hbar(2).to_tinybars()
     transfer_receipt = (
-        TransferTransaction()
+        transfer_tx
         .add_hbar_transfer(account.id, Hbar(1).to_tinybars())
         .add_hbar_transfer(env.operator_id, -Hbar(1).to_tinybars())
         .execute(env.client)
@@ -50,8 +52,10 @@ def test_integration_account_record_query_get_cost(env):
     """Test that AccountRecordsQuery can calculate query costs."""
     account = env.create_account()
 
+    transfer_tx = TransferTransaction()
+    transfer_tx.transaction_fee = Hbar(2).to_tinybars()
     transfer_receipt = (
-        TransferTransaction()
+        transfer_tx
         .add_hbar_transfer(account.id, Hbar(1).to_tinybars())
         .add_hbar_transfer(env.operator_id, -Hbar(1).to_tinybars())
         .execute(env.client)
@@ -75,8 +79,10 @@ def test_integration_account_record_query_insufficient_payment(env):
     """Test that AccountRecordsQuery fails with insufficient payment."""
     account = env.create_account()
 
+    transfer_tx = TransferTransaction()
+    transfer_tx.transaction_fee = Hbar(2).to_tinybars()
     transfer_receipt = (
-        TransferTransaction()
+        transfer_tx
         .add_hbar_transfer(account.id, Hbar(1).to_tinybars())
         .add_hbar_transfer(env.operator_id, -Hbar(1).to_tinybars())
         .execute(env.client)
