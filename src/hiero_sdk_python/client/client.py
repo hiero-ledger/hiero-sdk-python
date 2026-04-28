aca4aa960d08c27485c760357b27637d4b6f7104
Mohit Yadav <ymohit799057@gmail.com>
2026-04-29 01:00:59 +0530

    Close consensus node channels in client.py
    Ensure all consensus node channels are closed properly.
    
    Signed-off-by: Mohit Yadav <ymohit799057@gmail.com>

--- src/hiero_sdk_python/client/client.py
+++ src/hiero_sdk_python/client/client.py
@@ -210,7 +210,8 @@ class Client:
         # Fix: Close all consensus node channels
         if self.network and self.network.nodes:
             for node in self.network.nodes:
-
+                node._close()
+    
     def set_transport_security(self, enabled: bool) -> Client:
         """
         Enable or disable TLS for consensus node connections.
