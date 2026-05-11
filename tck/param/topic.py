a621641fcf62b049497cd97211f0b6e0e863a106
MonaaEid <monaa_eid@hotmail.com>
2026-05-12 02:02:21 +0300

    feat: implement createTopic functionality and related parameters in TCK handlers
    Signed-off-by: MonaaEid <monaa_eid@hotmail.com>

--- tck/param/topic.py
+++ tck/param/topic.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 
 from tck.param.base import BaseTransactionParams
 from tck.util.param_utils import (
-    non_empty_string_or_none,
     non_empty_string_list,
+    non_empty_string_or_none,
     parse_common_transaction_params,
     parse_session_id,
     to_bool,
