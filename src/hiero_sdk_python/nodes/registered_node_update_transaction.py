d19e0e07a4a32a3bfb088d7bcf30f06b8393e719
Manish Dait <daitmanish88@gmail.com>
2026-06-27 23:53:52 +0530

    chore: refactor the to_bytes/from_bytes method
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- src/hiero_sdk_python/nodes/registered_node_update_transaction.py
+++ src/hiero_sdk_python/nodes/registered_node_update_transaction.py
@@ -122,8 +122,8 @@ class RegisteredNodeUpdateTransaction(Transaction):
         return _Method(transaction_func=channel.address_book.updateRegisteredNode, query_func=None)
 
     @classmethod
-    def _from_protobuf(cls, transaction_body, body_bytes: bytes, sig_map):
-        transaction = super()._from_protobuf(transaction_body, body_bytes, sig_map)
+    def _from_protobuf(cls, transaction_body):
+        transaction = super()._from_protobuf(transaction_body)
 
         if transaction_body.HasField("registeredNodeUpdate"):
             pb = transaction_body.registeredNodeUpdate
