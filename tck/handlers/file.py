e8ed50940bbc3b698ca47842e7283627003b4e78
Achintya Sinha <achintyasin@gmail.com>
2026-08-26 17:07:25 +0530

    feat(tck): implement getFileInfo JSON-RPC method (#2580)
    Signed-off-by: achintya2k5 <achintyasin@gmail.com>
    Co-authored-by: exploreriii <133720349+exploreriii@users.noreply.github.com>

--- tck/handlers/file.py
+++ tck/handlers/file.py
@@ -3,17 +3,19 @@ from __future__ import annotations
 from hiero_sdk_python.file.file_contents_query import FileContentsQuery
 from hiero_sdk_python.file.file_create_transaction import FileCreateTransaction
 from hiero_sdk_python.file.file_id import FileId
+from hiero_sdk_python.file.file_info import FileInfo
+from hiero_sdk_python.file.file_info_query import FileInfoQuery
 from hiero_sdk_python.hbar import Hbar
 from hiero_sdk_python.response_code import ResponseCode
 from hiero_sdk_python.timestamp import Timestamp
 from hiero_sdk_python.transaction.transaction_receipt import TransactionReceipt
 from tck.errors import JsonRpcError
 from tck.handlers.registry import rpc_method
-from tck.param.file import CreateFileParams, GetFileContentsParams
-from tck.response.file import CreateFileResponse, GetFileContentsResponse
+from tck.param.file import CreateFileParams, GetFileContentsParams, GetFileInfoParams
+from tck.response.file import CreateFileResponse, GetFileContentsResponse, GetFileInfoResponse
 from tck.util.client_utils import get_client
 from tck.util.constants import DEFAULT_GRPC_TIMEOUT
-from tck.util.key_utils import get_key_from_string
+from tck.util.key_utils import get_key_from_string, key_to_string
 from tck.util.param_utils import to_int
 
 
@@ -76,3 +78,31 @@ def get_file_contents(params: GetFileContentsParams) -> GetFileContentsResponse:
     decoded_contents = contents.decode("utf-8", errors="replace") if isinstance(contents, bytes) else str(contents)
 
     return GetFileContentsResponse(contents=decoded_contents)
+
+
+def _build_file_info_response(info: FileInfo) -> GetFileInfoResponse:
+    """Build a GetFileResponse from a FileInfo object."""
+
+    keys = [key_to_string(k) for k in info.keys] if info.keys else []
+
+    return GetFileInfoResponse(
+        fileId=str(info.file_id) if info.file_id is not None else None,
+        size=str(info.size) if info.size is not None else None,
+        expirationTime=str(info.expiration_time.seconds) if info.expiration_time is not None else None,
+        isDeleted=info.is_deleted,
+        keys=keys,
+        memo=info.file_memo,
+        ledgerId=info.ledger_id.hex() if info.ledger_id is not None else None,
+    )
+
+
+@rpc_method("getFileInfo")
+def get_file_info(params: GetFileInfoParams) -> GetFileInfoResponse:
+    client = get_client(params.sessionId)
+    query = FileInfoQuery().set_grpc_deadline(DEFAULT_GRPC_TIMEOUT)
+
+    if params.fileId is not None:
+        query.set_file_id(FileId.from_string(params.fileId))
+
+    info = query.execute(client)
+    return _build_file_info_response(info)
